cb985876aeabe7291dc7c83da31db1b1a5515622
Shivansh Pratap <46789950+acousticclown@users.noreply.github.com>
2021-05-16 13:46:03 +0530

    React App Starter ✨

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import RecipesPage from "./Pages/RecipesPage";
+import "dotenv/config.js";
+
+function App() {
+  return (
+    <>
+      <RecipesPage />
+    </>
+  );
+}
+
+export default App;
